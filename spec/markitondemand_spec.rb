afae094e907fcf24f4be0d260422561f8746f6e4
Ray <ray@thelondonvandal.com>
2016-02-10 20:57:08 +0000

    fixed intendation

--- spec/markitondemand_spec.rb
+++ spec/markitondemand_spec.rb
@@ -1,14 +1,14 @@
 require 'spec_helper'
 
 describe Markitondemand do
-
   describe "company lookup API" do
-
     context "successful lookup request" do
-     before(:each) do
-       stub_request(:any, "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL").to_return(body: [{"Symbol"=>"AAPL", "Name"=>"Apple Inc", "Exchange"=>"NASDAQ"}].to_json)
-     end
-     let(:company) { Markitondemand::Company.new "AAPL" }
+      before(:each) do
+        response = [{ "Symbol" => "AAPL", "Name" => "Apple Inc", "Exchange" => "NASDAQ" }].to_json
+        url = "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL"
+        stub_request(:any, url).to_return(body: response)
+      end
+      let(:company) { Markitondemand::Company.new "AAPL" }
 
       it "returns a successful status" do
         expect(company.success?).to eq true
@@ -25,23 +25,20 @@ describe Markitondemand do
       it "returns the corresponding exchange" do
         expect(company.exchange).to eq "NASDAQ"
       end
-   end
+    end
 
-   context "unsuccessful lookup request" do
-       before(:each) do
-         stub_request(:any, "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL").to_return(status: 500)
-       end 
-       let(:company) { Markitondemand::Company.new "AAPL" }
+    context "unsuccessful lookup request" do
+      before(:each) do
+        stub_request(:any, "http://dev.markitondemand.com/MODApis/Api/v2/Lookup/json?input=AAPL").to_return(status: 500)
+      end
+      let(:company) { Markitondemand::Company.new "AAPL" }
 
-       it "sets the success field to false" do
-         expect(company.success?).to eq false
-       end
-
-   end
-
+      it "sets the success field to false" do
+        expect(company.success?).to eq false
+      end
+    end
   end
 
   describe "stock quote API" do
   end
-
 end
